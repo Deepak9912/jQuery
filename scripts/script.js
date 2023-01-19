65933873aca644f78820457be0c42ff78f7ffe58
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-01-19 21:27:47 +0000

    update jquery commands

--- scripts/script.js
+++ scripts/script.js
@@ -32,3 +32,62 @@ attribute filters
 $("div[class]".css({border: "2px sold red"})) - it takes all the fivs with class
 # any attribute can be used but it must be inside [] and attribute can be specified by [class=quote]
 */
+
+/*
+Traverse the DOM with jQuery
+
+next
+$("#contact-methods").next().css({border: "4px solid red"});
+
+prev
+$("#social-nav").prev().css({border: "3px solid red"})
+
+parent
+$(".banner-title").parent().css({border: "3px solid green"})
+
+Children
+$("#contact-methods").children().css({border: "3px solid green"})
+
+find
+$("#contact").find("img").css({border: "3px solid green"})
+
+closest
+# closest goes up the dom
+$("#social-nav").closest(".wrapper").css({border: "3px solid green"})
+
+*/
+
+/*
+jQuery Chaining
+to chain two or more commands together
+$("#contact-methods").css({border: "2px solid green"}).next().css({border: "2px solid black"})
+*/
+
+/*
+Adding content to the DOM
+
+var tweet = "<h1>Hello world!</h1>"
+
+$("#tweets div").append(tweet) - add at the bottom of variable
+$("#tweets div").prepend(tweet) - add at the top of variable
+$("#tweets div").before(tweet) - add before variable
+$("#tweets div").after(tweet) - add after variable
+$("#tweets div").html(tweet) - will remove all the content and replace with variable content
+*/
+
+
+/*
+Wrap and unwrap elements
+
+wrap() - wraps all matched elements individually
+upwrap() - unwrap all matched elements
+wrapAll() - wraps all elements combined with 1 single elements
+
+$("section").wrap("<div>");
+
+*/
+
+/*
+Changing attributes
+
+*/
